451a4e5697c225eac3a2fa2e9d09b58ba893ee86
charles <cgiguere@iname.com>
2023-12-08 06:21:29 -0500

    leftColumnPins

--- gpio_monitor.ino
+++ gpio_monitor.ino
@@ -3,7 +3,7 @@
 #include "secrets.h"
 
 // Define the GPIO pins to monitor
-const int gpioPins[] = {1,5, 8, 17, 16,9,10,11}; // Example GPIO pins
+const int gpioPins[] = {1, 5, 8, 17, 16, 9, 10, 11}; // Example GPIO pins
 const int numPins = sizeof(gpioPins) / sizeof(gpioPins[0]);
 
 GPIOMonitor gpio_monitor(gpioPins, numPins);
@@ -12,25 +12,25 @@ void setup()
 {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi...");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    Serial.println("Connecting to WiFi...");
+    Serial.print(".");
   }
   Serial.println("Connected to WiFi");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  pinMode(5,OUTPUT);
+  pinMode(5, OUTPUT);
 
   gpio_monitor.begin();
-
 }
 
 void loop()
 {
-  digitalWrite(5,HIGH);
+  digitalWrite(5, HIGH);
   delay(500);
-  digitalWrite(5,LOW);
+  digitalWrite(5, LOW);
   delay(500);
 }
