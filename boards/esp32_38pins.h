3e2026c8abaebeb6d5d1b7677e35b61ef89b77cf
charles <cgiguere@iname.com>
2023-12-08 06:54:55 -0500

    table_margins

--- boards/esp32_38pins.h
+++ boards/esp32_38pins.h
@@ -2,6 +2,5 @@ const int leftColumnPins[] = {INT_MAX,INT_MAX,36, 39, 34, 35, 32, 33, 25, 26, 27
 const int rightColumnPins[] = {INT_MAX, 23, 22,1, 3, 21,INT_MAX,19, 18, 5, 17, 16, 4, 0, 2, 15,8,7,6};
 const int numLeftColumnPins = sizeof(leftColumnPins) / sizeof(leftColumnPins[0]);
 const int numRightColumnPins = sizeof(rightColumnPins) / sizeof(rightColumnPins[0]);
-String margin_top = "50px";
-String margin_bottom = "50px";
+String table_margins = "margin-top: 50px; margin-bottom: 50px;";
 String table_border_spacing = "border-spacing: 0px 6px;";
