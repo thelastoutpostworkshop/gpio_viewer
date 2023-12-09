a312235bfec8a5ab4cb5e02d85193dcab6081322
charles <cgiguere@iname.com>
2023-12-09 11:19:14 -0500

    comments

--- src/gpio_viewer.h
+++ src/gpio_viewer.h
@@ -8,12 +8,14 @@
 #include "boards/esp32_S3_wroom_1.h"
 #include "boards/esp32_38pins.h"
 
+// Boards supported
 enum BoardType
 {
     ESP32_S3_WROOM_1 = 0,
     ESP32_38PINS = 1,
 };
 
+// Boards Definitions (must be in the same order as BoardType)
 ESPBoard board_models[] = {esp32_S3_wroom_1, esp32_38pins};
 
 class GPIOViewer
