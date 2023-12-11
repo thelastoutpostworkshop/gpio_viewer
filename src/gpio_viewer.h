34f9b81c01450a9f72798312c85e30a0fdf03176
charles <cgiguere@iname.com>
2023-12-11 18:41:04 -0500

    generateIndexHTML

--- src/gpio_viewer.h
+++ src/gpio_viewer.h
@@ -142,7 +142,7 @@ private:
         html += "<body><div class='grid-container'>\n";
 
         html += "<header class='header'>";
-        html += "<div class='centered-text' id='sampbox'>Sampling Interval is " + String(samplingInterval) + "ms\n" + board->getBoardModelName() + "</div>";
+        html += "<div class='centered-text' id='sampbox'>Sampling Interval is " + String(samplingInterval) + "ms</div>";
         html += "</header>";
 
         // Image
