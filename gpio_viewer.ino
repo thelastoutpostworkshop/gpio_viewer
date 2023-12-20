6358e7b7e064364a378c8d24654953c91258422a
charles <cgiguere@iname.com>
2023-12-20 15:41:48 -0500

    test

--- gpio_viewer.ino
+++ gpio_viewer.ino
@@ -7,18 +7,18 @@ GPIOViewer gpio_viewer;
 int test_digital_pins[] = {5, 4};
 const int testDigitalPinsCount = sizeof(test_digital_pins) / sizeof(test_digital_pins[0]);
 
-const int freq = 5000;
-const int resolution = 8;
+const int freq = 200;
+const int resolution = 16;
 
 struct PWM_PINS
 {
   int pin;
   int channel;
+  uint16_t level;
 };
 
-PWM_PINS test_pwm_pins[] = {{15, 0}, {2, 1},{0, 2},{4, 3}};
+PWM_PINS test_pwm_pins[] = {{15, 0}, {2, 1}, {0, 2}, {4, 3}};
 const int testPWMPinsCount = sizeof(test_pwm_pins) / sizeof(test_pwm_pins[0]);
-byte amount = 0;
 
 void setup()
 {
@@ -27,11 +27,14 @@ void setup()
   gpio_viewer.connectToWifi(ssid, password); // If your code aleady include connection to Wifi, you can comment this line
   gpio_viewer.setPort(5555);                 // You can set the http port, if not set default port is 8080
 
-  // Your own setup code start here
+  // Example - Your own setup code start here
+  uint16_t amount = 0;
   for (int i = 0; i < testPWMPinsCount; i++)
   {
+    amount += (65535 / testPWMPinsCount);
     ledcSetup(test_pwm_pins[i].channel, freq, resolution);
     ledcAttachPin(test_pwm_pins[i].pin, test_pwm_pins[i].channel);
+    test_pwm_pins[i].level = amount;
   }
 
   // Must be at the end of your setup
@@ -41,19 +44,16 @@ void setup()
 
 void loop()
 {
-  amount = 0;
   for (int i = 0; i < testPWMPinsCount; i++)
   {
-    amount+=(255/testPWMPinsCount);
-    ledcWrite(test_pwm_pins[i].channel, amount);
-    delay(300);
+    ledcWrite(test_pwm_pins[i].channel, test_pwm_pins[i].level);
+    delay(150);
   }
-  delay(1000);
+  delay(300);
   for (int i = 0; i < testPWMPinsCount; i++)
   {
-    amount+=(255/testPWMPinsCount);
-    ledcWrite(test_pwm_pins[i].channel, 0);
-    delay(300);
+    ledcWrite(test_pwm_pins[i].channel, test_pwm_pins[i].level / 2);
+    delay(150);
   }
-  delay(1000);
+  delay(300);
 }
