28a74cb335cde895d705a163ee9c0ddea8abaeb1
charles <cgiguere@iname.com>
2024-04-16 06:18:15 -0400

    comments

--- src/gpio_viewer.h
+++ src/gpio_viewer.h
@@ -124,7 +124,7 @@ public:
 
         if (checkWifiStatus())
         {
-            printPWNTraps();
+            // printPWNTraps();
             server = new AsyncWebServer(port);
 
             // Set CORS headers for global responses
