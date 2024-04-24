369612431448b0d1b8db3a1049e9e5e989d543c8
charles <cgiguere@iname.com>
2024-04-24 18:05:48 -0400

    test

--- gpio_viewer.ino
+++ gpio_viewer.ino
@@ -96,7 +96,7 @@ uint32_t getMaxDutyCycle(int resolution)
 
 void test1_setup()
 {
-  pinMode(INPUT_PIN,INPUT_PULLDOWN);
+  pinMode(INPUT_PIN,INPUT_PULLUP);
   uint16_t amount = 0;
   for (int i = 0; i < testPWMPinsCount; i++)
   {
