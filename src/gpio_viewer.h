7ea446af806abf88e836fd2081720e02324b9229
charles <cgiguere@iname.com>
2024-08-01 08:49:10 -0400

    v1.5.6

--- src/gpio_viewer.h
+++ src/gpio_viewer.h
@@ -22,7 +22,7 @@
 #endif
 #include <esp_partition.h>
 
-const char *release = "1.5.5";
+const char *release = "1.5.6";
 
 const String baseURL = "https://thelastoutpostworkshop.github.io/microcontroller_devkit/gpio_viewer_1_5/";
 
