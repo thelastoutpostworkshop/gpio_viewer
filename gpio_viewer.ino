cd9a72cac63076373ad6b26ac5bda1bd3f3aa575
charles <cgiguere@iname.com>
2024-11-29 14:59:16 -0500

    tested on ESP Async WebServer 3.3.23

--- gpio_viewer.ino
+++ gpio_viewer.ino
@@ -1,7 +1,7 @@
 // Test Code for the GPIOViewer development
 // Last tested on:
 // Espressif Arduino Core v3.0.7
-// ESP Async WebServer 3.3.22
+// ESP Async WebServer 3.3.23
 // AsyncTCP            1.1.4
 //
 #include "src/gpio_viewer.h"
