82497823f7066a7447f36816bf5e1b68220233d9
charles <cgiguere@iname.com>
2025-04-22 13:57:46 -0400

    1.6.3-unstable release

--- src/gpio_viewer.h
+++ src/gpio_viewer.h
@@ -18,7 +18,7 @@
 #endif
 #include <esp_partition.h>
 
-const char *release = "1.6.2";
+const char *release = "1.6.3-unstable release";
 
 const String baseURL = "https://thelastoutpostworkshop.github.io/microcontroller_devkit/gpio_viewer_1_5/";
 
